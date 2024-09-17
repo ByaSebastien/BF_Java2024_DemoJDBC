9914af62feb10245633148eb27c4b6abadb86fa3
Bya Sébastien <sebastien.bya@bstorm.be>
2024-09-17 10:03:53 +0200

    repo

--- src/main/java/be/bstorm/Main.java
+++ src/main/java/be/bstorm/Main.java
@@ -1,25 +1,17 @@
 package be.bstorm;
 
-import be.bstorm.entities.Author;
 import be.bstorm.entities.Book;
 import be.bstorm.repositories.BookRepository;
+import be.bstorm.repositories.impls.BookRepositoryImpl;
 
-import java.sql.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        BookRepository bookRepository = new BookRepository();
+        BookRepository bookRepository = new BookRepositoryImpl();
 
-        Book b = new Book("123",
-                "test",
-                "test...",
-                LocalDate.of(1991,3,27),
-                1);
 
-        bookRepository.save(b);
     }
 }
